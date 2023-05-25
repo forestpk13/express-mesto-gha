571d162fdbc7ac1b28dad7b9a83290995efacb2e
Anton Kostylev <forestpk13@gmail.com>
2023-05-25 22:34:20 +0700

    create user controller added

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,12 @@
 const users = require('express').Router();
+const { createUser } = require('../controllers/users');
 
-const User = require('../models/user');
+users.post('/', createUser);
 
-users.post('/', (req, res)=> {
-  const { name, about, avatar } = req.body;
-
-  User.create({ name, about, avatar})
-  .then(user => res.send({ data: user }))
-  .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+users.get('/:id', (req, res) => {
+  User.findById(req.params.id)
+    .then(user => res.send({ data: user }))
+    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
 });
 
 module.exports = users;
