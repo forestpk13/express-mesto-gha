57e8320564203881789761e643450cb78a275b79
Anton Kostylev <forestpk13@gmail.com>
2023-05-27 22:08:05 +0700

    user model fixes

--- models/user.js
+++ models/user.js
@@ -3,18 +3,21 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
+    default: 'Жак-Ив Кусто',
+    minlength: [2, 'поле должно содержать минимум 2 символа'],
+    maxlength: [30, 'максимальная длина поля 30 символов'],
     required: true,
   },
   about: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
+    default: 'Исследователь',
+    minlength: [2, 'поле должно содержать минимум 2 символа'],
+    maxlength: [30, 'максимальная длина поля 30 символов'],
     required: true,
   },
   avatar: {
     type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     required: true,
   },
 });
