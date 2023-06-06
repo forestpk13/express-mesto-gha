a1abe6df4e2ddc7b752b84459cc461ab3324d7a0
Anton Kostylev <forestpk13@gmail.com>
2023-06-06 23:01:23 +0700

    validaotor installed

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const validator = 0;
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -16,6 +18,20 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: ({ value }) => `${value} - некорректный адрес email`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
   },
 });
 
